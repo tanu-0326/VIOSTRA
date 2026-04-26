fb525674e007a5b6715dc2b31d6058c2ca232f8e
tanve <141725941+tanu-0326@users.noreply.github.com>
2026-04-26 20:28:22 +0530

    add user and video model

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error : ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(
+        `Application Running on http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
